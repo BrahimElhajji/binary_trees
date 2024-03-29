05235efa1d0556a97e871eb5d25c1c9d2083e8eb
ismailouzy <ismailouzal@gmail.com>
2024-03-28 21:09:27 -0700

    d

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,16 +7,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_h, right_h;
+	size_t left_h = 0, right_h = 0;
 
 	if (tree == NULL)
 		return (0);
+	if (tree->left)
+		left_h = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_h = 1 + binary_tree_height(tree->right);
 
-	left_h = binary_tree_height(tree->left);
-	right_h = binary_tree_height(tree->right);
+	return (left_h > right_h ? left_h : right_h);
 
-	if (left_h > right_h)
-		return (left_h + 1);
-	else
-		return (right_h + 1);
 }
